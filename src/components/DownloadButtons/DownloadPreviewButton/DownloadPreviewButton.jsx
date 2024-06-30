28bad9dbe469a4ed41f5a1bbf685f83da5092b7e
barisGultekin <alibarisgultekin@gmail.com>
2024-06-30 12:58:57 +0300

    Fix contact download buttons source and preview functionality

--- src/components/DownloadButtons/DownloadPreviewButton/DownloadPreviewButton.jsx
+++ src/components/DownloadButtons/DownloadPreviewButton/DownloadPreviewButton.jsx
@@ -1,25 +1,26 @@
 import React, { useState } from "react";
-
 import "./DownloadPreviewButton.css";
-
 import { IoClose } from "react-icons/io5";
 import { FaFileDownload } from "react-icons/fa";
 import { PiFrameCorners } from "react-icons/pi";
+import PopupPortal from "../../PopupPortal";
 
 const DownloadPreviewButton = ({ title, keyword }) => {
   const [showPopup, setShowPopup] = useState(false);
 
-  const imgUrl = `${process.env.PUBLIC_URL}` + "/" + keyword + ".webp";
+  const imgUrl =
+    `${process.env.PUBLIC_URL}` + "/aliBarisGultekin-" + keyword + ".webp";
+  const docUrl =
+    `${process.env.PUBLIC_URL}` + "/aliBarisGultekin-" + keyword + ".pdf";
 
   const handleClosePopup = () => {
     setShowPopup(false);
   };
 
-
   return (
     <div className="DownloadPreviewButton">
       <div className="downloadPreviewBox">
-        <a className="downloadButton" href="/resume.pdf" download>
+        <a className="downloadButton" href={docUrl} download>
           <FaFileDownload />
           Download {title}
         </a>
@@ -38,19 +39,22 @@ const DownloadPreviewButton = ({ title, keyword }) => {
       </div>
 
       {showPopup && (
-        <div className="popupDoc" onClick={handleClosePopup}>
-          <img src={imgUrl} alt="Popup" className="popup-image" />
+        <PopupPortal>
+          <div className="popupDoc">
+            <div className="popup-content" onClick={handleClosePopup}>
+              <div className="doc-close-button" onClick={handleClosePopup}>
+                <IoClose />
+              </div>
 
-          <div className="doc-close-button" onClick={handleClosePopup}>
-            <IoClose />
+              <img src={imgUrl} alt="Popup" className="popup-image" />
+
+              <a className="popup-downloadButton" href={docUrl} download onClick={(e) => e.stopPropagation()}>
+                <FaFileDownload />
+                Download {title}
+              </a>
+            </div>
           </div>
-
-          <a className="popup-downloadButton" href={`${process.env.PUBLIC_URL}/${keyword}.pdf`} download>
-
-            <FaFileDownload />
-            Download
-          </a>
-        </div>
+        </PopupPortal>
       )}
     </div>
   );
